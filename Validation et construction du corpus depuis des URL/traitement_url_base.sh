2f9003088338cc8fc4f37db6ec571cb78f446a8d
tanninaham <hamizi.tannina@gmail.com>
2022-11-09 11:18:04 +0100

    validation

---
+++ Validation et construction du corpus depuis des URL/traitement_url_base.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#===============================================================================
+# VOUS DEVEZ MODIFIER CE BLOC DE COMMENTAIRES.
+# Ici, on décrit le comportement du programme.
+# Indiquez, entre autres, comment on lance le programme et quels sont
+# les paramètres.
+# La forme est indicative, sentez-vous libres d'en changer !
+# Notamment pour quelque chose de plus léger, il n'y a pas de norme en bash.
+#===============================================================================
+
+fichier_urls=$1 # le fichier d'URL en entrée
+fichier_tableau=$2 # le fichier HTML en sortie
+
+# !!!!!!
+# ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
+# !!!!!!
+
+# modifier la ligne suivante pour créer effectivement du HTML
+echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
+
+lineno=1;
+echo $fichier_urls
+while read -r line;
+do
+	echo "ligne $lineno: $line";
+	lineno=$((lineno+1));
+done < "$fichier_urls"
