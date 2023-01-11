c1cc764711d6fe4d5b01f4b504da32779685c12e
tanninaham <hamizi.tannina@gmail.com>
2023-01-11 05:15:07 +0100

    ajout

---
+++ essai_website/source/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  // open modal
+  $('#open_modal').click(function(){
+    $('#modal_to_open').css(
+      {
+        'display': 'block'
+        <img alt="" source="img/ptit.gif" />
+      }
+    );
+  });
+
+  // close modal
+  $('#close_modal').click(function(){
+    $('#modal_to_open').css(
+      {
+        'display': 'none'
+      }
+    )
+  });
+});
